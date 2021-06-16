6f64f2c4db3f3266edfb3afe3aecf97cc7eae634
JanwillemWiltingh <123janwillem123@hotmail.nl>
2021-06-16 13:16:07 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+#   bot.py
+import os
+import sys
+import traceback
+from random import uniform, random, choice, sample
+
+import discord
+from discord.ext import commands
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+
+intents = discord.Intents.default()
+intents.members = True
+
+bot = commands.Bot(command_prefix='$', help_command=None, intents=intents)
